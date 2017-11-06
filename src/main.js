d8e5b4bd5095ccce23ea228f36493edb78450d7b
yhfhappy <2389081853@qq.com>
2017-11-06 15:51:47 +0800

    g
    g

--- src/main.js
+++ src/main.js
@@ -14,8 +14,8 @@ Vue.use(VueRouter);
 // 4.实例化一个路由
 const router = new VueRouter({
     /*去掉栏目名称前面的#号*/
-    mode:'history',
-    // mode:'hash',
+    // mode:'history',
+    mode:'hash',
 
     routes:routerConfig
 });
